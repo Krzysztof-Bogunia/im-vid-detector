a93f05784656dedfd32076e6f3a51a95b61cad46
Krzysztof Bogunia <krzysztof.bogunia.kontakt@gmail.com>
2025-09-30 16:21:32 +0200

    Initial program commit

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
-# im-vid-detector
-Image and video detector. Program can scan all media in folder using AI model and return only those that match specified target. Processing is done locally.
+# About:  
+Locally used image and video detection program. Images and videos can be scanned for any object so that they will be cropped by the program and detections copied to output folder.  
+Main features of this program: image/video detection of any class with cropping, automatic trimming and merging of video clips, efficient video processing (can do detection in less time than video duration and doesn't require 100+GB of RAM).  
+Default AI model for detection is "yoloe-11m-seg-pf.pt" or "yoloe-11m-seg.pt" depending on whether user specifies target text prompt.  
+When scanning images, each one is processed by AI model and if detection confidence is above threshold then image will be cropped to bounding box of detection and saved to output folder. Program will also save mask of the object detected with highest confidence.  
+Scanning videos is similar to images, but the difference is that only every N-th frame is processed and resulting videos are merged from clips of detections. Also masks are not created for videos.  
+Supported input file types are: .jpg, .png, .mp4, .mkv
+
+# Installation:  
+Use those commands to download program from github and install dependencies in virtual environment:  
+1. clone program's repository and go to main folder  
+   ```console
+   git clone https://github.com/Krzysztof-Bogunia/im-vid-detector.git
+   cd im-vid-detector
+    ```
+2. create venv  
+    ```console
+   python -m venv .venv
+    ```
+3. activate venv (for fish shell)*  
+   ```console
+   source .venv/bin/activate.fish
+    ```
+4. download pytorch (with cuda 12.9)  
+   ```console
+   pip install torch torchvision --index-url https://download.pytorch.org/whl/cu129
+    ```
+5. install the rest of dependencies  
+   ```console
+   pip install -r requirements.txt
+    ```  
+DONE
+* after program is installed only step 3 is required to activate environment and run it  
+
+# Examples: 
+1. Scanning image for any object with exact crop to detection:  
+```console
+python im-vid-detector.py --input ./examples/images/image0.jpg --crop_offset 0.0
+```
+|     BEFORE      |      AFTER     |
+| :-------------: | :------------: |
+| ![image0_before](./examples/images/image0.jpg)  | ![image0_after](./examples/results_images/image0.jpg) |  
+
+2. Scanning all images for "statue,monument,brick building,brickwork". Threshold for prompt detection often needs to be much lower than in prompt-less detection (especially for unusual objects):  
+```console
+python im-vid-detector.py --input ./examples/images/ --prompt "statue,monument" --threshold 0.02
+```
+|     BEFORE      |      AFTER     |
+| :-------------: | :------------: |
+| ![image2_before](./examples/images/image2.jpg)  | ![image2_after](./examples/results_images/image2.jpg) |  
+
+3. Scanning all images for "document".
+```console
+python im-vid-detector.py --input ./examples/images/ --prompt "document" --threshold 0.05
+```
+|     BEFORE      |      AFTER     |
+| :-------------: | :------------: |
+| ![image1_before](./examples/images/image1.jpg)  | ![image1_after](./examples/results_images/image1.jpg) | 
+
+4. Scanning all videos for "sword,magic weapon,medieval sword":  
+```console
+python im-vid-detector.py --input ./examples/videos/ --prompt "sword,magic weapon,medieval sword" --crop 1 --threshold 0.05 --crop_offset 0.02 --frame_skip 2 --model yoloe-11l-seg.pt
+```
+|     BEFORE      |      AFTER     |
+| :-------------: | :------------: |
+| <video src="./examples/videos/video0.mp4" controls></video> | <video src="./examples/results_videos/video0.mp4" controls></video> | 
+
+
+For prompt keywords check this list of predefined classes [classes](https://github.com/xinyu1205/recognize-anything/blob/main/ram/data/ram_tag_list.txt)
---
+++ im-vid-detector.py
@@ -0,0 +1,390 @@
+import os
+import numpy as np
+import cv2
+from ultralytics import YOLOE
+from ultralytics.models.yolo.yoloe import YOLOEVPSegPredictor
+from datetime import datetime
+import ffmpeg
+import time
+import shutil 
+import argparse
+
+def detect_objects(model, image, DETECT_THRESHOLD=0.2):
+    # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = model.predict(image, conf=DETECT_THRESHOLD, verbose=False)
+    b_mask = np.zeros(image.shape[:2], np.uint8)
+    bbox = []
+    score = 0.0
+    # results[0].show()
+    if(results[0].masks != None):
+        contour = results[0].masks.xy.pop().astype(np.int32).reshape(-1, 1, 2)
+        xyxy = results[0].boxes.xyxy.cpu().numpy().squeeze().astype(np.int32)
+        if(len(xyxy.shape) == 1):
+            bbox = xyxy
+        else:
+            bbox = [np.min(xyxy, axis=0)[0], np.min(xyxy, axis=0)[1], np.max(xyxy, axis=0)[2], np.max(xyxy, axis=0)[3]]
+        _ = cv2.drawContours(b_mask, [contour], -1, (255, 255, 255), cv2.FILLED)
+        score = results[0].boxes.conf[0].item()
+    mask = b_mask
+    return mask, bbox, score
+
+def save_image(image, path):
+    _path = path
+    if os.path.exists(_path):
+        filename, extension = os.path.splitext(_path)
+        iter = 1
+        while os.path.exists(_path):
+            _path = filename + " (" + str(iter) + ")" + extension
+            iter = iter + 1
+    cv2.imwrite(_path, image)
+    
+def process_frame(model, image, DETECT_THRESHOLD=0.2):
+    h,w = image.shape[0:2]
+    image2 = image
+    h2,w2 = image2.shape[0:2]
+    MAX_RES = 640*2
+    TARGET_HEIGHT = 640
+    interpolation=cv2.INTER_NEAREST
+    do_scaling = True
+    if(w > (MAX_RES)):
+        w2 = MAX_RES
+        h2 = h*MAX_RES/w
+    elif((w < (640)) and (h < (TARGET_HEIGHT))):
+        w2 = 640
+        h2 = h*640/w
+    if(h2 > (TARGET_HEIGHT*2)):
+        w2 = w2*TARGET_HEIGHT*2.0/h2
+        h2 = TARGET_HEIGHT*2
+    if(do_scaling):
+        image2 = cv2.resize(image2, 
+                            dsize=(int(w2), int(h2)), 
+                            interpolation=interpolation)
+    
+    detected = False
+    mask, bbox, score = detect_objects(model, image2, DETECT_THRESHOLD)
+    if(score >= DETECT_THRESHOLD):
+        detected = True
+        mask = cv2.resize(mask, 
+                            dsize=(w, h), 
+                            interpolation=cv2.INTER_NEAREST)
+        bbox[0] = int(bbox[0] * (w/w2))
+        bbox[1] = int(bbox[1] * (h/h2))
+        bbox[2] = int(bbox[2] * (w/w2))
+        bbox[3] = int(bbox[3] * (h/h2))
+    return detected, mask, bbox, score
+
+def bboxToRange(CROP_SIZE_OFFSET, h, w, bbox):
+    _CROP_SIZE_OFFSET = CROP_SIZE_OFFSET
+    if((_CROP_SIZE_OFFSET > 0.0) and (_CROP_SIZE_OFFSET < 1.0)):
+        _CROP_SIZE_OFFSET = int(_CROP_SIZE_OFFSET * min([w,h]))
+    if((_CROP_SIZE_OFFSET < 0.0) and (_CROP_SIZE_OFFSET > -1.0)):
+        _CROP_SIZE_OFFSET = int(_CROP_SIZE_OFFSET * min([w,h]))
+    _CROP_SIZE_OFFSET = int(_CROP_SIZE_OFFSET)
+    x1, y1, x2, y2 = bbox
+    x1 = max({x1-_CROP_SIZE_OFFSET, 0})
+    y1 = max({y1-_CROP_SIZE_OFFSET, 0})
+    x2 = min({x2+_CROP_SIZE_OFFSET, w})
+    y2 = min({y2+_CROP_SIZE_OFFSET, h})
+    return x1,y1,x2,y2
+
+def video_cut_and_merge_detections(MEDIA_PATH, file, detections, boxes, w, h, frame_rate, DO_CROP, CROP_SIZE_OFFSET=0, TEMP_PATH="./temp/"):
+    filename, extension = os.path.splitext(file)
+    detected_ranges = []
+    start_frame = 0
+    end_frame = 0
+    previous = False
+    boxes2 = []
+    box1 = []
+    if(not any(detections)):
+        return
+    
+    for i in range(0, len(detections), 1):
+        frame_n = i*FRAME_SKIP
+        if((detections[i] == True) and (previous == False)):
+            start_frame = frame_n
+            box1 = boxes[i]
+        elif( ((detections[i] == False) and (previous == True)) or
+            ( (detections[i] == True) and (i == (len(detections)-1))) ):
+            end_frame = frame_n
+            detected_ranges.append([start_frame, end_frame])
+            box2 = boxes[i-1]
+            bbox = [min(box1[0],box2[0]), min(box1[1],box2[1]), max(box1[2],box2[2]), max(box1[3],box2[3])]
+            boxes2.append(bbox)
+        previous = detections[i]
+        
+    if not os.path.exists(TEMP_PATH):
+        os.makedirs(TEMP_PATH)
+    clips = []
+    max_width = 0
+    max_height = 0
+    if(DO_CROP):
+        for i in range(0, len(detected_ranges)):
+            bbox = boxes2[i]
+            x1, y1, x2, y2 = bboxToRange(CROP_SIZE_OFFSET, h, w, bbox)
+            width = (x2-x1)
+            height = (y2-y1)
+            max_width = max(max_width, width)
+            max_height = max(max_height, height)
+    # handle width,height must be divisible by 2 for ffmpeg
+    max_width = int(int(max_width/2) * 2)
+    max_height = int(int(max_height/2) * 2)
+    temp_files = []
+    has_audio = False
+    has_video = False
+    w2 = w
+    h2 = h
+    num_clips = 0
+    for i in range(0, len(detected_ranges)):
+        t1 = detected_ranges[i][0]/frame_rate
+        t2 = detected_ranges[i][1]/frame_rate
+        try:
+            clip = ffmpeg.input(MEDIA_PATH+file)
+            video = clip.video.filter('trim', start=t1, end=t2).setpts('PTS-STARTPTS')
+            audio = clip.audio
+            has_audio = False
+            has_video = False
+            audio_probe = ffmpeg.probe(MEDIA_PATH+file, select_streams='a')
+            video_probe = ffmpeg.probe(MEDIA_PATH+file, select_streams='v')
+            if audio_probe['streams']:
+                has_audio = True
+            if video_probe['streams']:
+                has_video = True
+            if(not has_video):
+                continue
+            if(has_audio):
+                audio = audio.filter('atrim', start=t1, end=t2).filter('asetpts', 'PTS-STARTPTS')
+            if(DO_CROP):
+                num_processing = 0
+                bbox = boxes2[i]
+                x1, y1, x2, y2 = bboxToRange(CROP_SIZE_OFFSET, h, w, bbox)
+                width = (x2-x1)
+                height = (y2-y1)
+                # handle width,height must be divisible by 2 for ffmpeg
+                width = int(int(width/2) * 2)
+                height = int(int(height/2) * 2)
+                centerx = x1+int(width/2)
+                centery = y1+int(height/2)
+                num_processing = num_processing+1
+                video_path = TEMP_PATH+"part"+str(num_clips)+"_video"+str(num_processing)+extension
+                video = video.crop(x1,y1,width,height)
+                (
+                    ffmpeg
+                    .output( video, filename=video_path, loglevel="quiet")
+                    .overwrite_output()
+                    .run()
+                )
+                video = ffmpeg.input(video_path).video
+                
+                # if((width != max_width) and (height != max_height)):
+                if True:
+                    w2 = int(max_width)
+                    h2 = int(height*max_width/width)
+                    if(abs(max_width - width) > abs(max_height-height)):
+                        h2 = int(max_height)
+                        w2 = int(width*max_height/height)
+                    if(w2 > max_width):
+                        h2 = int(h2*max_width/w2)
+                        w2 = int(max_width)
+                    if(h2 > max_height):
+                        w2 = int(w2*max_height/h2)
+                        h2 = int(max_height)
+                    # handle width,height must be divisible by 2 for ffmpeg
+                    w2 = int(int(w2/2) * 2)
+                    h2 = int(int(h2/2) * 2)
+                    num_processing = num_processing+1
+                    video_path = TEMP_PATH+"part"+str(num_clips)+"_video"+str(num_processing)+extension
+                    time.sleep(1)
+                    (
+                        ffmpeg
+                        .output( video, filename=video_path, vf="scale="+str(w2)+":"+str(h2)+",setsar="+str(1), loglevel="quiet")
+                        .overwrite_output()
+                        .run()
+                    )
+                    video = ffmpeg.input(video_path).video
+                    
+                if((width != max_width) or (height != max_height)):
+                    video = video.filter('pad', width=int(max_width), height=int(max_height), x=int(-1), y=int(-1))
+            if(has_audio):
+                (
+                    ffmpeg
+                    .output(video, audio, TEMP_PATH+"part"+str(num_clips)+extension, loglevel="quiet")
+                    .overwrite_output()
+                    .run()
+                )
+            else:
+                (
+                    ffmpeg
+                    .output(video, TEMP_PATH+"part"+str(num_clips)+extension, loglevel="quiet")
+                    .overwrite_output()
+                    .run()
+                )
+            num_clips = num_clips+1
+        except:
+            print("error when processing fragment of: "+file + ". Skipping clip.")
+    #merge partial clips
+    streams = []
+    partial_files = []
+    for i in range(0, num_clips):
+        clip = ffmpeg.input(TEMP_PATH+"part"+str(i)+extension)
+        streams.append(clip)
+        partial_files.append(clip.video)
+        if (has_audio):
+            partial_files.append(clip.audio)
+    if (has_audio):
+        concatenated = (
+            ffmpeg
+            .concat(*partial_files, v=1, a=1)
+            .node
+        )
+        (
+            ffmpeg
+            .output(concatenated[0], concatenated[1], OUTPUT_MEDIA_PATH + file, loglevel="quiet")
+            .overwrite_output()
+            .run()
+        )
+    else:
+        concatenated = (
+            ffmpeg
+            .concat(*partial_files, v=1)
+            .node
+        )
+        (
+            ffmpeg
+            .output(concatenated[0], OUTPUT_MEDIA_PATH + file, loglevel="quiet")
+            .overwrite_output()
+            .run()
+        )
+        
+    if os.path.exists(TEMP_PATH):
+        shutil.rmtree(TEMP_PATH) 
+    return OUTPUT_MEDIA_PATH + file
+
+if __name__ == "__main__":
+    
+    INPUT_PATH = "./input/"
+    MASK_SAVE_PATH = "./output/masks/"
+    OUTPUT_MEDIA_PATH = "./output/media/"
+    TEMP_PATH = "./temp/"
+    DETECTION_TEXTS = [""]
+    DO_CROP = True
+    DETECT_THRESHOLD = 0.7 # 0.02 0.05 0.17 0.7
+    CROP_SIZE_OFFSET = 0.04
+    FRAME_SKIP = 30
+    MODEL_NAME = "yoloe-11m-seg-pf.pt" #"yoloe-11m-seg.pt" "yoloe-11m-seg-pf.pt"
+    
+    parser = argparse.ArgumentParser(description="img_vid_masker")
+    parser.add_argument("--input", help="input media path. Default value: ./input/", default=INPUT_PATH)
+    parser.add_argument("--masks", help="output masks path. Default value: ./output/masks/", default=MASK_SAVE_PATH)
+    parser.add_argument("--output_media", help="output processed media path. Default value: ./output/media/", default=OUTPUT_MEDIA_PATH)
+    parser.add_argument("--temp", help="output temporary media path (*CAN BE AUTOMATICALLY DELETED!*). Default value: ./temp/", default=TEMP_PATH)
+    parser.add_argument("--prompt", help="target text description. Default value is empty so model should detect most likely class in input image", default=DETECTION_TEXTS[0])
+    parser.add_argument("--crop", help="whether to crop input images to size matching bounding box of detection {0;1}. Default value: 1", default=DO_CROP)
+    parser.add_argument("--threshold", help="detection confidence threshold <0; 1>. Default value: 0.7", default=DETECT_THRESHOLD)
+    parser.add_argument("--crop_offset", help="detection bounding box crop size offset defined as ratio of pixels <-1; 1>. Default value: 0.04", default=CROP_SIZE_OFFSET)
+    parser.add_argument("--frame_skip", help="how many video frames to skip in each iteration of detection. Default value: 30", default=FRAME_SKIP)
+    parser.add_argument("--model", help="name of the model for detection. Default value: yoloe-11m-seg-pf.pt (without text prompt) or yoloe-11m-seg.pt (with text prompt)", default=MODEL_NAME)
+    
+    args = parser.parse_args()
+    # parser.print_help()
+    if args.input is not None:
+        INPUT_PATH = str(args.input)
+    if args.masks is not None:
+        MASK_SAVE_PATH = str(args.masks)
+    if args.output_media is not None:
+        OUTPUT_MEDIA_PATH = str(args.output_media)
+    if args.temp is not None:
+        TEMP_PATH = str(args.temp)
+    if args.prompt is not None:
+        DETECTION_TEXTS = [str(args.prompt)]
+    if args.crop is not None:
+        DO_CROP = bool(int(args.crop))
+    if args.threshold is not None:
+        DETECT_THRESHOLD = float(args.threshold)
+    if args.crop_offset is not None:
+        CROP_SIZE_OFFSET = float(args.crop_offset)
+    if args.frame_skip is not None:
+        FRAME_SKIP = int(args.frame_skip)
+    if args.model is not None:
+        MODEL_NAME = str(args.model)
+    
+    start = datetime.now()
+    model = None
+    if(len(DETECTION_TEXTS[0]) > 0):
+        MODEL_NAME = MODEL_NAME.replace("-pf", "")
+        model = YOLOE(MODEL_NAME)
+        model.set_classes(DETECTION_TEXTS, model.get_text_pe(DETECTION_TEXTS))
+    else:
+        MODEL_NAME = MODEL_NAME.replace(".pt", "-pf.pt")
+        MODEL_NAME = MODEL_NAME.replace("-pf-pf", "-pf")
+        model = YOLOE(MODEL_NAME)
+    
+    MEDIA_PATH = ""
+    num_files = 0
+    num_images = 0
+    num_videos = 0
+    num_detections = 0
+    files = []
+    if(os.path.isdir(INPUT_PATH)):
+        MEDIA_PATH = INPUT_PATH + str("/")
+        MEDIA_PATH = MEDIA_PATH.replace("//", "/")
+        files = os.listdir(MEDIA_PATH)
+        num_files = len(files)
+        print("Input directory has "+str(num_files)+" files")
+    else:
+        MEDIA_PATH = os.path.dirname(INPUT_PATH) + str("/")
+        MEDIA_PATH = MEDIA_PATH.replace("//", "/")
+        files = [os.path.basename(INPUT_PATH)]
+    for file in files:
+        if (file.endswith(".png") or file.endswith(".jpg")):
+            num_images = num_images+1
+            image = cv2.imread(MEDIA_PATH+file)
+            h,w = image.shape[0:2]
+            detected, mask, bbox, score = process_frame(model, image, DETECT_THRESHOLD)
+            if(detected):
+                num_detections = num_detections+1
+                
+                if not os.path.exists(MASK_SAVE_PATH):
+                    os.makedirs(MASK_SAVE_PATH)
+                if not os.path.exists(OUTPUT_MEDIA_PATH):
+                    os.makedirs(OUTPUT_MEDIA_PATH)
+                output_image = image
+                if(DO_CROP):
+                    x1, y1, x2, y2 = bboxToRange(CROP_SIZE_OFFSET, h, w, bbox)
+                    output_image = output_image[y1:y2, x1:x2]
+                save_image(mask, MASK_SAVE_PATH + file)
+                save_image(output_image, OUTPUT_MEDIA_PATH + file)
+                
+        if(file.endswith(".mp4") or file.endswith(".mkv")):
+            num_videos = num_videos+1
+            detections = []
+            boxes = []
+            cap = cv2.VideoCapture(MEDIA_PATH+file)
+            frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+            frame_rate = float(cap.get(cv2.CAP_PROP_FPS))
+            h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+            for i in range(0, frame_count, FRAME_SKIP):
+                frame_n = i
+                cap.set(cv2.CAP_PROP_POS_FRAMES, frame_n-1)
+                res, image = cap.read()
+                detected, mask, bbox, score = process_frame(model, image, DETECT_THRESHOLD)
+                if(detected):
+                    num_detections = num_detections+1
+                    detections.append(True)
+                    boxes.append(bbox)
+                else:
+                    detections.append(False)
+                    boxes.append(bbox)
+
+            if(len(boxes) > 0):
+                video_cut_and_merge_detections(MEDIA_PATH, file, detections, boxes, w, h, frame_rate, DO_CROP, CROP_SIZE_OFFSET=0, TEMP_PATH="./temp/")
+
+
+    stop = datetime.now()
+    print("Processed "+str(num_images)+" images")
+    print("Processed "+str(num_videos)+" videos")
+    print("Detected "+str(num_detections)+" objects")
+    print("Elapsed time = "+str(stop-start)+" [h][m][s]")
+
+    
+
